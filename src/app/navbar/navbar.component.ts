18ba8f2747ed5cb652cf17b5c5af16d0c4d41488
Ali Hatem <68661572+alihatem360@users.noreply.github.com>
2022-07-30 14:34:03 +0200

    add bootstrap & jquery popper.js

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  name = 'Angular';
+  img = "assets/morty.jpg";
+  
   constructor() { }
 
   ngOnInit(): void {
