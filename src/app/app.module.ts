5e666071551bbd6c13ba9df0f3a4d481a608f0b7
Ali Hatem <68661572+alihatem360@users.noreply.github.com>
2022-08-01 20:01:58 +0200

    upload files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { LolComponent } from './lol/lol.component';
+
+
+
+const routes: Routes = [
+  { path: 'menu', component: MenuComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: HeaderComponent },
+  { path: 'home', component: HeaderComponent },
+  
+];
 
 
 @NgModule({
@@ -13,14 +27,17 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     NavbarComponent,
     HeaderComponent,
- 
+    AboutComponent,
+    MenuComponent,
+    LolComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    RouterModule.forRoot(routes),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
