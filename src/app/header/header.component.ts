777f90e0bd724f5cdb6905bf1a2ed289873b8025
Ali Hatem <68661572+alihatem360@users.noreply.github.com>
2022-08-02 17:49:03 +0200

    up load files

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NewsService } from '../news.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
 
-  allNews = [];
-  constructor( public _NewsService: NewsService) {
-    this.allNews = this._NewsService.getNews();
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+export class HeaderComponent {
+
+  constructor() {
+  
   }
 
-   }
-
-  ngOnInit(): void {
-  }
-
+  
 }
--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -7,10 +7,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NewsService {
 
-  constructor(public _HttpClient: HttpClient) { 
-    getNews(){
-return console.log("ccccc")    }
+  constructor(public _HttpClient: HttpClient) {
   }
+
+  getNews() {
+    return this._HttpClient.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=3b9c1c3c9b3c4c3c9b3c4c3c9b3c4c3c')
+  }
+  
 }
+
+
 
 
